203c8058dd658442dd58c7cd7094633bc4cf7354
JoseLuis2510 <josejb425@gmail.com>
2025-03-28 13:47:23 -0600

    Update ConexionBD.php

--- ConexionBD.php
+++ ConexionBD.php
@@ -1,16 +1,6 @@
 <?php
-$host = 'vindascraft.mysql.database.azure.com';
-$username = 'muebleriavindas';
-$password = 'Proyecto2025*.';
-$db_name = 'muebleria';
-
-$conn = mysqli_init();
+$con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "./DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
-
-if (mysqli_connect_errno($conn)){
-die ('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-
-mysqli_close($conn);
+mysqli_real_connect($conn, "vindascraft.mysql.database.azure.com", "muebleriavindas", "Proyecto2025*.", "muebleria", 3306, MYSQLI_CLIENT_SSL);
 ?>
+
