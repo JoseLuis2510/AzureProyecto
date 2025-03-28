90b79cc386b5830f6d7335508767b5eea0269908
JoseLuis2510 <josejb425@gmail.com>
2025-03-27 21:43:06 -0600

    Update ConexionBD.php

--- ConexionBD.php
+++ ConexionBD.php
@@ -2,6 +2,6 @@
 <?php
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "vindascraft.mysql.database.azure.com", "muebleriavindas", "Proyecto2025*.", "muebleria", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($con, "vindascraft.mysql.database.azure.com", "muebleriavindas", "Proyecto2025*.", "muebleria", 3306, MYSQLI_CLIENT_SSL);
 ?>
 
