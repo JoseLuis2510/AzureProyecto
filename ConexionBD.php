6222a991a52ad31b4a02495304eee50e543fb757
JoseLuis2510 <josejb425@gmail.com>
2025-03-28 11:49:44 -0600

    Update ConexionBD.php

--- ConexionBD.php
+++ ConexionBD.php
@@ -1,23 +1,5 @@
 <?php
-// Datos de la base de datos
-$servidor = "vindascraft.mysql.database.azure.com";  // Asegúrate que este es el servidor correcto
-$usuario = "muebleriavindas";  // Usuario con privilegios para acceder a la base de datos
-$contraseña = "Proyecto2025*.";  // La contraseña correcta
-$base_datos = "muebleria";  // El nombre de la base de datos que estás utilizando
-
-// Crear la conexión
-$conn = mysqli_init();
-
-// Configurar SSL
-mysqli_ssl_set($conn, NULL, NULL, "./DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-
-// Intentar la conexión
-if (!mysqli_real_connect($conn, $servidor, $usuario, $contraseña, $base_datos, 3306, NULL, MYSQLI_CLIENT_SSL)) {
-    echo('❌ Error al conectar a MySQL: ' . mysqli_connect_error());
-} else {
-    echo "✅ Conexión exitosa a MySQL en Azure";
-}
-
-// Cerrar la conexión al final
-mysqli_close($conn);
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "./DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+mysqli_real_connect($con, "vindascraft.mysql.database.azure.com", "muebleriavindas", "Proyecto2025*.", "muebleriavindas", 3306, MYSQLI_CLIENT_SSL);
 ?>
