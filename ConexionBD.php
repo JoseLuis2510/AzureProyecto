c741b7dc7fb463af36d3b7a9921a959d49563ea6
JoseLuis2510 <josejb425@gmail.com>
2025-03-28 07:56:25 -0600

    ConexionBD
    ConexionBD

--- ConexionBD.php
+++ ConexionBD.php
@@ -1,18 +1,11 @@
 <?php
-// Habilitar la visualización de errores
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 $con = mysqli_init();
-mysqli_ssl_set($con, NULL, NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+mysqli_ssl_set($con, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 
-// Usar la misma variable $con en mysqli_real_connect
-if (mysqli_real_connect($con, "vindascraft.mysql.database.azure.com", "muebleriavindas", "Proyecto2025*.", "muebleria", 3306, NULL, MYSQLI_CLIENT_SSL)) {
-    echo "Conexión exitosa a la base de datos MySQL en Azure!";
-} else {
-    echo "Error en la conexión: " . mysqli_connect_error();
+if (!mysqli_real_connect($con, "vindascraft.mysql.database.azure.com", "muebleriavindas", "Proyecto2025*.", "muebleria", 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
 
-// Cerrar la conexión
-mysqli_close($con);
+echo "Conexión exitosa a la base de datos en Azure";
+
 ?>
